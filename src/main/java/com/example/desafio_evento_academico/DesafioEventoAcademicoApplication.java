5589a6676d407ba3ae7af17d104fe816031314cb
Eduardo Melo <eduardo.melo01992@gmail.com>
2024-12-19 13:48:16 -0300

    projeto criado

---
+++ src/main/java/com/example/desafio_evento_academico/DesafioEventoAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_evento_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEventoAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEventoAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_evento_academico/DesafioEventoAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_evento_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEventoAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
